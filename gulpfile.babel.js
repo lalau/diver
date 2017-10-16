d5cfb0fd18d79c6660fd93be873decb0f07da150
Lawrence Lau <ktslwy@gmail.com>
2017-10-16 01:08:59 -0700

    v1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,53 +3,62 @@ import path from 'path';
 import del from 'del';
 import webpack from 'webpack';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
 
-const chromeDevConfig = {
-    entry: {
-        diver: './wrappers/chrome/diver.js',
-        sandbox: './wrappers/chrome/sandbox.js'
-    },
-    output: {
-        path: path.join(__dirname, './dist/chrome/'),
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel-loader'],
-                include: [
-                    path.resolve(__dirname, 'src'),
-                    path.resolve(__dirname, 'wrappers')
-                ]
-            },
-            {
-                test: /\.s?css$/,
-                loaders: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true
+const getBaseConfig = () => {
+    return {
+        entry: {
+            diver: './wrappers/chrome/diver.js',
+            sandbox: './wrappers/chrome/sandbox.js'
+        },
+        output: {
+            filename: '[name].js'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loaders: ['babel-loader'],
+                    include: [
+                        path.resolve(__dirname, 'src'),
+                        path.resolve(__dirname, 'wrappers')
+                    ]
+                },
+                {
+                    test: /\.s?css$/,
+                    loaders: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
                         }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ],
-                include: [
-                    path.resolve(__dirname, 'styles'),
-                    path.resolve(__dirname, 'node_modules/react-table')
-                ]
-            }
+                    ],
+                    include: [
+                        path.resolve(__dirname, 'styles'),
+                        path.resolve(__dirname, 'node_modules/react-table')
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CopyWebpackPlugin([{from: './wrappers/chrome/'}], {ignore: ['diver.js', 'sandbox.js']})
         ]
-    },
-    plugins: [
-        new CopyWebpackPlugin([{from: './wrappers/chrome/'}], {ignore: ['diver.js', 'sandbox.js']})
-    ]
+    };
 };
+
+const chromeDevConfig = getBaseConfig();
+chromeDevConfig.output.path = path.join(__dirname, './dist/chrome/dev');
+
+const chromeProdConfig = getBaseConfig();
+chromeProdConfig.output.path = path.join(__dirname, './dist/chrome/prod');
+chromeProdConfig.plugins.push(new UglifyJSPlugin());
 
 gulp.task('clean', (done) => {
     del(['dist/']).then(() =>{
@@ -59,4 +68,8 @@ gulp.task('clean', (done) => {
 
 gulp.task('build:chrome:dev', ['clean'], (done) => {
     webpack(chromeDevConfig, done);
+});
+
+gulp.task('build:chrome:prod', ['clean'], (done) => {
+    webpack(chromeProdConfig, done);
 });
